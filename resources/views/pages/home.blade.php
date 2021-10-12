0c71ffec2d14da577a40034fdec5a464380593a0
Macluffy <ahiriga.a@hotmail.com>
2021-10-12 19:33:04 +0200

    debutprojet

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,29 @@
+@extends('template/welcome')
+
+@section('content')
+    @include('section/header')
+    @include('section/areastart')
+    @include('section/about')
+    @include('section/class')
+    @include('section/schedule')
+    @include('section/trainer')
+    @include('section/gallerie')
+    @include('section/event')
+    @include('section/pricing')
+    @include('section/client')
+    @include('section/map')
+    @include('section/footer')
+@endsection
+		
+        
+       
+        
+       
+        
+        
+        
+        
+        
+        
+        
+		
---
+++ resources/views/section/header.blade.php
@@ -0,0 +1,52 @@
+<!-- Header Area Start -->
+<header class="top">
+    <div class="header-area ptb-18 header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-xs-12">
+                    <div class="logo">
+                        <a href="index.html"><img src="img/logo/logo.png" alt="COFFEE" /></a>
+                    </div>
+                </div>
+                <div class="col-md-8 col-xs-12">
+                    <div class="content-wrapper">
+                        <!-- Main Menu Start -->
+                        <div class="main-menu text-center">
+                            <nav>
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="about-us.html">About us</a></li>
+                                    <li><a href="menu.html">classes</a></li>
+                                    <li><a href="reservation.html">gallery</a></li>
+                                    <li><a href="blog.html">blog</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="mobile-menu hidden-lg hidden-md"></div>
+                        <!-- Main Menu End -->
+                    </div>
+                </div>
+                <div class="col-md-2 hidden-sm hidden-xs">
+                    <div class="header-contact text-right">
+                        <a class="banner-btn" data-text="contact" href="contact.html"><span>contact</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header Area End -->
+@if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
